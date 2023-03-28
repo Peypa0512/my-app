e64e3b30a159787eef03043dfe7f3a56d45fb51f
peypa0512 <pedro.sanzr@outlook.es>
2023-03-28 16:37:58 +0200

    Nombre del Proyecto: Proyecto Prueba  Descripción: APP con Maven

---
+++ src/main/java/com/pedro/myapp/entity/TestEntity.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.pedro.myapp.entity;
+
+/**
+ * @author Pedro 
+ *
+ */
+public class TestEntity {
+	/**
+	 * prueba de muestra de complicación de archivo testentity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+}
